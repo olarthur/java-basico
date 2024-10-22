288c95c00e6f2d4fa2b96a48c4e597f2e2923efd
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-22 19:13:20 -0300

    Update DivisaoFrame.java

--- OperadoresSwing/src/DivisaoFrame.java
+++ OperadoresSwing/src/DivisaoFrame.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 
 public class DivisaoFrame extends JFrame {
 
-    // Componentes da interface
     private JLabel labelNumerador;
     private JLabel labelDenominador;
     private JTextField campoNumerador;
@@ -15,15 +14,11 @@ public class DivisaoFrame extends JFrame {
     private JLabel resultadoDivisao;
     private JLabel resultadoResto;
 
-    // Construtor
     public DivisaoFrame() {
-        // Definindo o título da janela
         super("Divisão com Resto");
 
-        // Configurando o layout
         setLayout(null);
 
-        // Criação dos componentes
         labelNumerador = new JLabel("Numerador:");
         labelDenominador = new JLabel("Denominador:");
         campoNumerador = new JTextField(10);
@@ -34,7 +29,6 @@ public class DivisaoFrame extends JFrame {
         resultadoDivisao = new JLabel("0");
         resultadoResto = new JLabel("0");
 
-        // Definindo posições dos componentes
         labelNumerador.setBounds(30, 30, 100, 25);
         campoNumerador.setBounds(150, 30, 100, 25);
         labelDenominador.setBounds(30, 70, 100, 25);
@@ -45,7 +39,6 @@ public class DivisaoFrame extends JFrame {
         labelResto.setBounds(30, 190, 100, 25);
         resultadoResto.setBounds(150, 190, 100, 25);
 
-        // Adicionando os componentes ao frame
         add(labelNumerador);
         add(campoNumerador);
         add(labelDenominador);
@@ -56,20 +49,17 @@ public class DivisaoFrame extends JFrame {
         add(labelResto);
         add(resultadoResto);
 
-        // Ação do botão "Dividir"
         botaoDividir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    // Obtendo os valores de entrada
+
                     int numerador = Integer.parseInt(campoNumerador.getText());
                     int denominador = Integer.parseInt(campoDenominador.getText());
 
-                    // Realizando as operações de divisão e resto
                     int divisao = numerador / denominador;
                     int resto = numerador % denominador;
 
-                    // Atualizando os rótulos com os resultados
                     resultadoDivisao.setText(String.valueOf(divisao));
                     resultadoResto.setText(String.valueOf(resto));
 
@@ -81,13 +71,11 @@ public class DivisaoFrame extends JFrame {
             }
         });
 
-        // Configurações da janela
         setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    // Método principal para rodar a aplicação
     public static void main(String[] args) {
         new DivisaoFrame();
     }
