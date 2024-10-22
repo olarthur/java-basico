a63245ec439e6c756b41cf404cf23c54a7b71ce0
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-22 19:45:24 -0300

    Update OperadoresAritmeticos.java

--- OperadoresAritmeticos/src/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/OperadoresAritmeticos.java
@@ -1,10 +1,16 @@
 public class OperadoresAritmeticos {
     public static void main(String[] args) throws Exception {
         
+        /* 
         int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2) / 2;
         System.out.println("A media e igual a " + m);
+        */
 
+
+        int numero = 5;
+        numero++;
+        System.out.println(numero);
     }
 }
