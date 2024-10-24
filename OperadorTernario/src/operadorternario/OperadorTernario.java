3f6372671fe8be63ae425dd037edce6ffa2f7091
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-24 17:04:05 -0300

    opeTer

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,14 @@
+package operadorternario;
+public class OperadorTernario {
+    public static void main(String[] args) throws Exception {
+
+        int n1, n2, r;
+
+        n1 = 14;
+        n2 = 18;
+
+        r = (n1 > n2) ? n1 + n2 : n1 - n2;
+
+        System.out.println(r);
+    }
+}
