54b84596dc0874d97a8cfebcf2bf2e318bd3178c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-25 19:41:17 -0300

    sc

---
+++ SwitchCase/src/programapernas/Pernas.java
@@ -0,0 +1,32 @@
+package programapernas;
+
+import java.util.Scanner;
+public class Pernas {
+    public static void main(String[] args) throws Exception {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Quantas pernas? ");
+        int pernas = leia.nextInt();
+        String tipo;
+        System.out.println("Isso é um(a): ");
+
+        switch (pernas) {
+            case 1: 
+            tipo = "Saci";
+            break;
+            case 2: 
+            tipo = "Bípede";
+            break;
+            case 4: 
+            tipo = "Quadrúpede";
+            break;
+            case 6, 8:
+            tipo = "Aranha";
+            break;
+            default: 
+            tipo = "ET";
+        }
+        System.out.println(tipo);
+
+    }
+}
