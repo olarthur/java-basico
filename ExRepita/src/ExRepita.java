2c1380cc0077f5b170bbbd553aa7d8fe86040260
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-26 20:01:14 -0300

    Update ExRepita.java

--- ExRepita/src/ExRepita.java
+++ ExRepita/src/ExRepita.java
@@ -3,13 +3,25 @@ import javax.swing.JOptionPane;
 public class ExRepita {
     public static void main(String[] args) throws Exception {
  
-        int n;
+        int n, totV = 0, par = 0, impar = 0, hun = 0, media;
 
         do {
 
-        n = Integer.parseInt (JOptionPane.showInputDialog(null, "Informe um Número: (O valor 0 interrompe)"));
+        n = Integer.parseInt (JOptionPane.showInputDialog(null, "<html>Informe um Número: <br><em>(O valor 0 interrompe)</em></html>"));
+        
+        if (n % 2 == 0) {
+            if (n != 0)
+            par++;
+        } else {
+            impar++;
+        }
 
+        if(n > 100) {
+            hun++;
+        }
+        
         } while (n != 0);
-
+        JOptionPane.showMessageDialog(null, "<html>Resultado Final <br><hr>" + "Total de Pares: " + par +  "<br>Total de Ímpares: " + impar + "</html>");
+       
     }
 }
