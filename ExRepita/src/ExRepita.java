4addc7cfa070faa287863fe949db885e1d7517c5
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-26 20:33:10 -0300

    Update ExRepita.java

--- ExRepita/src/ExRepita.java
+++ ExRepita/src/ExRepita.java
@@ -2,26 +2,36 @@ import javax.swing.JOptionPane;
 
 public class ExRepita {
     public static void main(String[] args) throws Exception {
- 
-        int n, totV = 0, par = 0, impar = 0, hun = 0, media;
-
+        int n, totV = 0, par = 0, impar = 0, hun = 0, s = 0;
+        double media = 0; 
         do {
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "<html>Informe um Número: <br><em>(O valor 0 interrompe)</em></html>"));
 
-        n = Integer.parseInt (JOptionPane.showInputDialog(null, "<html>Informe um Número: <br><em>(O valor 0 interrompe)</em></html>"));
-        
-        if (n % 2 == 0) {
-            if (n != 0)
-            par++;
-        } else {
-            impar++;
-        }
+            if (n != 0) { 
+                totV++; 
+                s += n; 
 
-        if(n > 100) {
-            hun++;
-        }
-        
+                if (n % 2 == 0) {
+                    par++; 
+                } else {
+                    impar++; 
+                }
+
+                if (n > 100) {
+                    hun++;
+                }
+            }
         } while (n != 0);
-        JOptionPane.showMessageDialog(null, "<html>Resultado Final <br><hr>" + "Total de Pares: " + par +  "<br>Total de Ímpares: " + impar + "</html>");
-       
+
+        media = totV > 0 ? (double) s / totV : 0;
+
+        JOptionPane.showMessageDialog(null, "<html>Resultado Final <br><hr>" +
+                "Total de Valores: " + totV + 
+                "<br>Total de Pares: " + par +  
+                "<br>Total de Ímpares: " + impar + 
+                "<br>Acima de 100: " + hun + 
+                "<br>A soma é: " + s + 
+                "<br>Média dos valores: " + media + 
+                "</html>");
     }
 }
