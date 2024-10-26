d31c2c2864359a34b612fad217d4fa0a1377e57f
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-26 18:58:22 -0300

    Create - Repeticao01

---
+++ Repeticao01/src/Repeticao.java
@@ -0,0 +1,10 @@
+public class Repeticao {
+    public static void main(String[] args) throws Exception {
+
+        int cc = 0;
+        do {
+            System.out.println("Spin");
+            cc++;
+        } while(cc < 4);
+    }
+}
