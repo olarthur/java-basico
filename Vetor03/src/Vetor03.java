6eb6fa8025178c4e1a82ec7a8ee96767544f5513
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 18:05:53 -0300

    Update

--- Vetor03/src/Vetor03.java
+++ Vetor03/src/Vetor03.java
@@ -1,7 +1,11 @@
-public class App {
+import java.util.Arrays;
+
+public class Vetor03 {
     public static void main(String[] args) throws Exception {
 
         double v[] = {1.1, 5.4, -6.3, 3.7, -7.8}; 
+
+        Arrays.sort(v);;
         for (double valor : v){ 
             System.out.print(valor + " ");
         }
