f4fec1ca147b880cd26ee7a8eab35b5c6ee6bc49
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 16:04:42 -0300

    Create - RepeticaoFor.java

---
+++ RepeticaoFor/src/RepeticaoFor.java
@@ -0,0 +1,8 @@
+public class RepeticaoFor {
+    public static void main(String[] args) throws Exception {
+
+         for (int cc = 0; cc < 3; cc++) {
+            System.out.println("Spin");
+         }
+    }
+}
