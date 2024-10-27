de8b79448caeb458e344e22526e0990b7b1d0ee5
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 18:02:13 -0300

    Create - Vetor03.java

---
+++ Vetor03/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        double v[] = {1.1, 5.4, -6.3, 3.7, -7.8}; 
+        for (double valor : v){ 
+            System.out.print(valor + " ");
+        }
+    }
+}
