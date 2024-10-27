d7af9dbf6a851aa684d1c438fe06c5de3cfb4bc7
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 17:40:20 -0300

    Create - Vetor01.java

---
+++ Vetor01/src/Vetor01.java
@@ -0,0 +1,10 @@
+public class Vetor01 {
+    public static void main(String[] args) throws Exception {
+
+        int n[] = {2, 3, 4, 5, 6, 8};
+
+        for (int c = 0; c <= n.length - 1; c++ ) {
+            System.out.println("Na posição " + c + " temos o valor: " + n[c]);
+        }
+    }
+}
