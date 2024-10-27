976ea8e68a51914d711cf3b3a990aa9b68b754a2
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 19:24:08 -0300

    Create - Funcao01.java

---
+++ Funcao01/src/Funcao01.java
@@ -0,0 +1,13 @@
+public class Funcao01 {
+
+    static int soma(int a, int b) {
+        int s = a + b;
+        return s;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int sm = soma(5,2);
+        System.out.println("A soma vale: " + sm);
+    }
+}
