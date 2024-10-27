7d0c5605e6a6e637a57b13453cd66cc4574f11a4
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-27 17:54:06 -0300

    Update

--- Vetor02/src/Vetor02.java
+++ Vetor02/src/Vetor02.java
@@ -1,6 +1,11 @@
 public class Vetor02 {
     public static void main(String[] args) throws Exception {
 
-        
+        String mes[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Set", "Oct", "Nov", "Dec"};
+        int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int c = 0; c < mes.length; c++) {
+            System.out.println("O mês de " + mes[c] + " tem " + tot[c] + " dias.");
+        }
     }
 }
