5e02bccfaa2083444493809ae65ab66cd403fa9d
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-31 17:31:50 -0300

    Update Aluno.java

--- Aula_5/src/aula5/Aluno.java
+++ Aula_5/src/aula5/Aluno.java
@@ -1,2 +1,89 @@
 package aula5;
 
+public class Aluno implements Aula{
+    private String nome;
+    private int idade;
+    private String ra;
+    private String cpf;
+    private String curso;
+    private double notaAv1;
+    private double notaAv2;
+    
+    
+    public Aluno(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = "000.000.000-00";
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double getNotaAv1() {
+        return notaAv1;
+    }
+
+    public void setNotaAv1(double notaAv1) {
+        this.notaAv1 = notaAv1;
+    }
+
+    public double getNotaAv2() {
+        return notaAv2;
+    }
+
+    public void setNotaAv2(double notaAv2) {
+        this.notaAv2 = notaAv2;
+    }
+
+    @Override
+    public void av1() {
+        
+    }
+
+    @Override
+    public void av2() {
+        
+    }
+
+    @Override
+    public void entradaCatraca() {
+        
+    }
+}
