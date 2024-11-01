c7d687414bcdbbe3f8e5adcfbad34b5e30addfa7
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-01 10:42:00 -0300

    Update Aula5.java

--- Aula_5/src/aula5/Aula5.java
+++ Aula_5/src/aula5/Aula5.java
@@ -3,7 +3,14 @@ package aula5;
 public class Aula5 {
 
     public static void main(String[] args) {
+        Aluno estudante1 = new Aluno("Arthur", "444.444.444-41");
         
+        estudante1.setNome("Arthur Oliveira");
+        estudante1.av1(7.5);
+        estudante1.av2(5.5);
+        
+        estudante1.exibirNotas();
+        //System.out.println(estudante1.getNome());
     }
     
 }
