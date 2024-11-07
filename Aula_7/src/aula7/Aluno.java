73ca7a3ebe04f53ff4093dcbfae7be564e6d497c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-07 13:12:17 -0300

    Create Aluno.java

---
+++ Aula_7/src/aula7/Aluno.java
@@ -0,0 +1,9 @@
+package aula7;
+
+public class Aluno extends Pessoa{
+    //Método Construtor da Classa Mãe
+    public Aluno(String nome, String cpf, String data_nascimento) {
+        super(nome, cpf, data_nascimento);
+    }
+    
+}
