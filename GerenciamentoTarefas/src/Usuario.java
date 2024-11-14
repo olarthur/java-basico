5bbbde3210ea8dfa54b74b80f27c5a645874aa00
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-11-14 16:06:34 -0300

    Update Usuario.java

--- GerenciamentoTarefas/src/Usuario.java
+++ GerenciamentoTarefas/src/Usuario.java
@@ -1,3 +1,23 @@
 public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String papel;
 
+    public Usuario(int id, String nome, String papel) {
+        this.id = id;
+        this.nome = nome;
+        this.papel = papel;
+    }
+
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getPapel() { return papel; }
+    public void setPapel(String papel) { this.papel = papel; }
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", nome=" + nome + ", papel=" + papel + "]";
+    }
 }
